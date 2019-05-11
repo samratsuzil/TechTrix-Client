4cfda9db55201faef9c7d0d5345f874f5576c3e1
bornjre <bornjre@gmail.com>
2019-05-12 02:33:12 +0545

    added utilities function

---
+++ src/service/utilities.js
@@ -0,0 +1,100 @@
+/*
+let staticdata = {
+  "ejeh6d2la-ZeRhhDahMedYUCOoPUhZpHS6YYG6XkNgg=": {
+    HashID: "ejeh6d2la-ZeRhhDahMedYUCOoPUhZpHS6YYG6XkNgg=",
+    TransactionInfo: {
+      Subject: "child one",
+      Body: " This is child one",
+      CreatedAt: 1557596584,
+      Amount: 1000,
+      User: "Karmarchari32"
+    },
+    ParentHash: "uKG6FBvzEqf6d9Sz4XiMj1-ilCbLWKiVmTEEF23AjLc="
+  },
+  "uKG6FBvzEqf6d9Sz4XiMj1-ilCbLWKiVmTEEF23AjLc=": {
+    HashID: "uKG6FBvzEqf6d9Sz4XiMj1-ilCbLWKiVmTEEF23AjLc=",
+    TransactionInfo: {
+      Subject: "Root Node",
+      Body: "This is root node all nodes of seven kingdom",
+      CreatedAt: 1557596584,
+      Amount: 10000,
+      User: "Karmarchari32"
+    },
+    ParentHash: ""
+  },
+  "ukP80BcdjewEL3QtGeohc8bvAZGGicImfrNMg-hKdDM=": {
+    HashID: "ukP80BcdjewEL3QtGeohc8bvAZGGicImfrNMg-hKdDM=",
+    TransactionInfo: {
+      Subject: "child two",
+      Body: " This is child two",
+      CreatedAt: 1557596584,
+      Amount: 1000,
+      User: "Karmarchari32"
+    },
+    ParentHash: "uKG6FBvzEqf6d9Sz4XiMj1-ilCbLWKiVmTEEF23AjLc="
+  },
+  "z9Z2jNhDCsuorR84wPnQxGRMfReNglkzK5GWFniGYoA=": {
+    HashID: "z9Z2jNhDCsuorR84wPnQxGRMfReNglkzK5GWFniGYoA=",
+    TransactionInfo: {
+      Subject: "grandchild one",
+      Body: " This is gchild one",
+      CreatedAt: 1557596584,
+      Amount: 1000,
+      User: "Karmarchari32"
+    },
+    ParentHash: "ejeh6d2la-ZeRhhDahMedYUCOoPUhZpHS6YYG6XkNgg="
+  }
+}; */
+
+export default function TransformData(data) {
+  //t["attributes"] = t["TransactionInfo"];
+  /*
+  let allval = {};
+
+  Object.entries(data).forEach(val => {
+    val["attributes"] = val["TransactionInfo"];
+
+    allval[val.HashID] = val;
+  });
+  */
+
+  let _data = Object.values(data);
+
+  return listToTree(_data, {
+    idKey: "HashID",
+    parentKey: "ParentHash"
+  });
+}
+
+function listToTree(data, options) {
+  options = options || {};
+  var ID_KEY = options.idKey || "id";
+  var PARENT_KEY = options.parentKey || "parent";
+  var CHILDREN_KEY = options.childrenKey || "children";
+
+  var tree = [],
+    childrenOf = {};
+  var item, id, parentId;
+
+  for (var i = 0, length = data.length; i < length; i++) {
+    item = data[i];
+    id = item[ID_KEY];
+    parentId = item[PARENT_KEY] || "";
+    // every item may have children
+    childrenOf[id] = childrenOf[id] || [];
+    // init its children
+    item[CHILDREN_KEY] = childrenOf[id];
+    if (parentId != "") {
+      // init its parent's children object
+      childrenOf[parentId] = childrenOf[parentId] || [];
+      // push it into its parent's children object
+      childrenOf[parentId].push(item);
+    } else {
+      tree.push(item);
+    }
+  }
+
+  return tree;
+}
+
+//console.log(TransformData(staticdata));
--- src/views/Graph/Graph.jsx
+++ src/views/Graph/Graph.jsx
@@ -1,18 +1,66 @@
-import React, { Component } from 'react';
-class Graph extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return (
-            <div className="content">
-            hello
-            </div>
+import React, { Component } from "react";
+import Tree from "react-d3-tree";
+import Api from "service/Api";
+import TransformData from "service/utilities.js";
 
+const api = new Api();
 
-         );
-    }
+const myTreeData = [
+  {
+    name: "Top Level",
+    attributes: {
+      keyA: "val A",
+      keyB: "val B",
+      keyC: "val C"
+    },
+    children: [
+      {
+        name: "Level 2: A",
+        attributes: {
+          keyA: "val A",
+          keyB: "val B",
+          keyC: "val C"
+        }
+      },
+      {
+        name: "Level 2: B"
+      }
+    ]
+  }
+];
+
+export default class Graph extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transactions: []
+    };
+    this.getGraph = this.getGraph.bind(this);
+  }
+  componentDidMount() {
+    this.getGraph();
+  }
+
+  getGraph() {
+    api.getData("/transactions").then(res => {
+      //console.log(res.data);
+
+      let t = TransformData(res.data);
+      console.log(t);
+
+      this.setState({
+        transactions: t
+      });
+    });
+  }
+  //this.state.transactions
+  render() {
+    return (
+      <div className="content">
+        <div id="treeWrapper" style={{ width: "100em", height: "50em" }}>
+          <Tree data={myTreeData} />
+        </div>
+      </div>
+    );
+  }
 }
- 
-export default Graph;
